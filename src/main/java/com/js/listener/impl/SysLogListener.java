95a07e515c95e45dfe5f1e175457ea3ddb0c8f7c
渡劫 <2545251075@qq.com>
2021-05-31 08:56:12 +0800

    fix -> 清理多余的工具类避免重复代码，SpringUtil在Hutool里有封装

--- src/main/java/com/js/listener/impl/SysLogListener.java
+++ src/main/java/com/js/listener/impl/SysLogListener.java
@@ -1,8 +1,8 @@
 package com.js.listener.impl;
 
+import cn.hutool.extra.spring.SpringUtil;
 import com.js.controller.TestController;
 import com.js.listener.ExcelListener;
-import com.js.util.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
