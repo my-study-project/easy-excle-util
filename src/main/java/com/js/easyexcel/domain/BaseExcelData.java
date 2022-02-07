231022dd48ced73de423917c759e3b2e84023856
姜爽 <2545251075@qq.com>
2022-02-07 22:07:20 +0800

    工具类修正

--- src/main/java/com/js/easyexcel/domain/BaseExcelData.java
+++ src/main/java/com/js/easyexcel/domain/BaseExcelData.java
@@ -6,21 +6,22 @@ import com.alibaba.excel.annotation.write.style.ContentStyle;
 import com.alibaba.excel.annotation.write.style.HeadFontStyle;
 import com.alibaba.excel.annotation.write.style.HeadRowHeight;
 import com.alibaba.excel.annotation.write.style.HeadStyle;
-import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.VerticalAlignment;
+import com.alibaba.excel.enums.BooleanEnum;
+import com.alibaba.excel.enums.poi.BorderStyleEnum;
+import com.alibaba.excel.enums.poi.FillPatternTypeEnum;
+import com.alibaba.excel.enums.poi.HorizontalAlignmentEnum;
+import com.alibaba.excel.enums.poi.VerticalAlignmentEnum;
 
 /**
  * Excel基本设置
  */
-@HeadStyle(fillPatternType = FillPatternType.SOLID_FOREGROUND, fillForegroundColor = 30,
-        horizontalAlignment = HorizontalAlignment.CENTER, verticalAlignment = VerticalAlignment.CENTER,
-        wrapped = true, shrinkToFit = true,
-        borderTop = BorderStyle.NONE, borderBottom = BorderStyle.NONE, borderLeft = BorderStyle.NONE, borderRight = BorderStyle.NONE)
+@HeadStyle(fillPatternType = FillPatternTypeEnum.SOLID_FOREGROUND, fillForegroundColor = 30,
+        horizontalAlignment = HorizontalAlignmentEnum.CENTER, verticalAlignment = VerticalAlignmentEnum.CENTER,
+        wrapped = BooleanEnum.TRUE, shrinkToFit = BooleanEnum.TRUE,
+        borderTop = BorderStyleEnum.NONE, borderBottom = BorderStyleEnum.NONE, borderLeft = BorderStyleEnum.NONE, borderRight = BorderStyleEnum.NONE)
 @HeadRowHeight(24)
-@HeadFontStyle(bold = true, color = 9, fontName = "Arial", fontHeightInPoints = 10)
-@ContentStyle(horizontalAlignment = HorizontalAlignment.CENTER, verticalAlignment = VerticalAlignment.CENTER)
+@HeadFontStyle(bold = BooleanEnum.TRUE, color = 9, fontName = "Arial", fontHeightInPoints = 10)
+@ContentStyle(horizontalAlignment = HorizontalAlignmentEnum.CENTER, verticalAlignment = VerticalAlignmentEnum.CENTER)
 @ContentRowHeight(19)
 @ContentFontStyle(fontName = "Arial", fontHeightInPoints = 10)
 public abstract class BaseExcelData {
